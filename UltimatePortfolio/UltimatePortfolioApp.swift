1f71dc950e21e2c4cd7788e22758365ce5a60411
Steve Webber <steve@gibbethill.com>
2020-10-29 08:25:20 -0400

    Next commit

---
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,26 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Steven Webber on 10/23/20.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    @StateObject var dataController: DataController
+    
+    init() {
+        let dataController = DataController()
+        _dataController = StateObject(wrappedValue: dataController)
+    }
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, dataController.container.viewContext)
+                .environmentObject(dataController)
+        }
+    }
+}
